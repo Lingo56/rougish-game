d7a0e2ef46715e439f63eeaf15850b399bed585b
Constructor <firelight.thelegit@gmail.com>
2015-11-29 01:38:47 -0800

    Arithmetic ops now return 1.

--- spec/lib-infinityShim.lua
+++ spec/lib-infinityShim.lua
@@ -12,29 +12,29 @@ createShim = function(options)
       return newShim
     end,
     __unm = function(t)
-      return t
+      return 1
     end,
     __add = function(t)
-      return t
+      return 1
     end,
     __sub = function(t)
-      return t
+      return 1
     end,
     __mul = function(t)
-      return t
+      return 1
     end,
     __div = function(t)
-      return t
+      return 1
     end,
     __mod = function(t)
-      return t
+      return 1
     end,
     __pow = function(t)
-      return t
+      return 1
     end,
     __concat = function(t)
-      return t
-    end,
+      return 1
+    end
   }
   if options.isWeak then shimMetaTable.__mode = 'kv' end
   setmetatable(shim, shimMetaTable)
