ba0bf86f5c5445293dd0ca6b5a8de588236364ce
Constructor <firelight.thelegit@gmail.com>
2015-11-29 01:31:59 -0800

    Really fixed inifinityShim.

--- spec/lib-infinityShim.lua
+++ spec/lib-infinityShim.lua
@@ -1,22 +1,15 @@
 local createShim
-
-local function newShim(t, k, options)
-  local newShim = createShim(options)
-  t[k] = newShim
-  return newShim
-end
-
 createShim = function(options)
   options = options or {}
   local shim = {}
   local shimMetaTable = {
-    __call = options.callFunc or function(t, k)
+    __call = options.callFunc or function(t)
+      return t
+    end,
+    __index = function(t, k)
       local newShim = createShim(options)
       t[k] = newShim
       return newShim
-    end,
-    __index = function(t, k)
-      return newShim(t, k, options)
     end
   }
   if options.isWeak then shimMetaTable.__mode = 'kv' end
