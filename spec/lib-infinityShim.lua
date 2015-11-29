59d359fddee1d7ce94b7e5c23de80fbd453c67c3
Howard Nguyen <Luminess@users.noreply.github.com>
2015-11-29 01:21:50 -0800

    Calling shim now returns another shim.

--- spec/lib-infinityShim.lua
+++ spec/lib-infinityShim.lua
@@ -1,13 +1,20 @@
 local createShim
+
+local function newShim(t, k, options)
+  local newShim = createShim(options)
+  t[k] = newShim
+  return newShim
+end
+
 createShim = function(options)
   options = options or {}
   local shim = {}
   local shimMetaTable = {
-    __call = options.callFunc or function() end,
+    __call = options.callFunc or function(t, k)
+      return newShim(t, k, options)
+    end,
     __index = function(t, k)
-      local newShim = createShim(options)
-      t[k] = newShim
-      return newShim
+      return newShim(t, k, options)
     end
   }
   if options.isWeak then shimMetaTable.__mode = 'kv' end
