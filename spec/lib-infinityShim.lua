f828d352381c6fea31b7c2e2a484da182da3dd02
Constructor <firelight.thelegit@gmail.com>
2015-11-29 01:36:18 -0800

    Infinity shim now supports arithmetic.

--- spec/lib-infinityShim.lua
+++ spec/lib-infinityShim.lua
@@ -10,7 +10,31 @@ createShim = function(options)
       local newShim = createShim(options)
       t[k] = newShim
       return newShim
-    end
+    end,
+    __unm = function(t)
+      return t
+    end,
+    __add = function(t)
+      return t
+    end,
+    __sub = function(t)
+      return t
+    end,
+    __mul = function(t)
+      return t
+    end,
+    __div = function(t)
+      return t
+    end,
+    __mod = function(t)
+      return t
+    end,
+    __pow = function(t)
+      return t
+    end,
+    __concat = function(t)
+      return t
+    end,
   }
   if options.isWeak then shimMetaTable.__mode = 'kv' end
   setmetatable(shim, shimMetaTable)
