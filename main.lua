6bfc5f629b7410df258ee5619d2e30fd40982c15
Howard Nguyen <howardf.ho@CLYH-LIBR-W013.sd36.bc.ca>
2015-12-04 14:20:42 -0800

    Wipe

--- main.lua
+++ main.lua
@@ -2,13 +2,9 @@ io.stdout:setvbuf('no')
 print('\n')
 
 _G.tiny = require('lib.vendor.tiny')
-_G.gamestate = require('lib.vendor.hump.gamestate')
-
-local Game = require('src.states.Game')
 
 function love.load()
   love.mouse.setVisible(false)
-  gamestate.registerEvents()
-	gamestate.switch(Game())
-  love.resize(love.graphics.getWidth(), love.graphics.getHeight())
+  
+
 end
