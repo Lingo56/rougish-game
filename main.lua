4d28d14522e5af713b79e32d8afdeef08c83eaea
Howard Nguyen <Luminess@users.noreply.github.com>
2015-12-07 12:58:53 -0800

    Continue rework.

--- main.lua
+++ main.lua
@@ -1,10 +1,28 @@
 io.stdout:setvbuf('no')
 print('\n')
 
-_G.tiny = require('lib.vendor.tiny')
+require('lib.vendor.lovetoys.lovetoys')
+_G.lovetoyDebug = true
+
+local class = _G.class
+local engine, events
+_G.engine = engine
+_G.events = events
 
 function love.load()
   love.mouse.setVisible(false)
-  
+  engine = Engine()
+  world = love.physics.newWorld(0, 0, true)
+  world:setCallbacks(beginContact, endContact)
+  events = EventManager()
+  love.mouse.setVisible(true)
+end
 
+function love.update(deltaTime)
+  engine:update(deltaTime)
+  world:update(deltaTime)
+end
+
+function love.draw()
+  engine:draw()
 end
